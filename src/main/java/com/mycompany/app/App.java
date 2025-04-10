7e55c133523051dbcd40be0c9108909f452991fa
Ashley Wang <ashleywangsm013@gmail.com>
2025-04-10 09:52:47 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is coveted af");
+        System.out.println("Testing Github webook.");
     }
 
     public String getMessage() {
